890b9fb17cf27c7c3aad1fc740a0f6caafeb9ae6
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-02 22:38:58 +0530

    Create frowen.py

---
+++ Frowen/frowen.py
@@ -0,0 +1,19 @@
+# (c) Lx 0980
+# Yeat : 2023
+
+from Frowen import API_ID, API_HASH, BOT_TOKEN
+    
+from pyrogram import Client
+
+frowen = Client(
+  "frowen"
+  api_id = API_ID,
+  api_hash = API_HASH,
+  bot_token = BOT_TOKEN,
+  plugins = {
+  'root': 'Frowen.plugins'
+  }
+)
+
+
+frowen.run()
