147463331cb428ed5b08356a8ee2e1fff503510a
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-02 23:02:19 +0530

    Update __main__.py

--- Frowen/__main__.py
+++ Frowen/__main__.py
@@ -2,18 +2,21 @@
 # Yeat : 2023
 
 from Frowen import API_ID, API_HASH, BOT_TOKEN
-    
+
 from pyrogram import Client
 
 frowen = Client(
-  "frowen"
-  api_id = API_ID,
-  api_hash = API_HASH,
-  bot_token = BOT_TOKEN,
-  plugins = {
-  'root': 'Frowen.plugins'
-  }
+    "frowen",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={
+        'root': 'Frowen.plugins'
+    }
 )
+
+frowen.run()
+
 
 
 frowen.run()
