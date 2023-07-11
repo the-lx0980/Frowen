bce1173601fc099d58c4852b3b6d3b00ff6926f0
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-11 18:53:09 +0530

    Update __init__.py

--- Frowen/__init__.py
+++ Frowen/__init__.py
@@ -6,3 +6,4 @@ from os import environ
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+USER_SESSION = environ.get("BOT_TOKEN")
